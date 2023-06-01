ac296d04fd8d490b9034d69168ca415c19d09a2e
GmeloG <goncalomelogoncalves@gmail.com>
2023-06-01 23:12:49 +0100

    adicinar javascript

--- BilheiteiraOnline/Scripts/index.js
+++ BilheiteiraOnline/Scripts/index.js
@@ -1,3 +1,24 @@
-function onClickHome(){
-    window.location.href="index.html";
+class UsernamePassword {
+  constructor(email, password, name) {
+    this.email = email;
+    this.password = password;
+    this.name = name;
+  }
 }
+
+function onClickLogin(){
+   let user = new UsernamePassword("utilizador","dsasa","utilizador");
+   
+   if (user.name == "utilizador"){    
+        if (user.password == "dsasa"){
+            alert("Bem vindo "+user.name);
+            windows.location.href="index.html";
+        }
+   }
+}
+function hideLogin(){
+
+
+
+}
+ 
