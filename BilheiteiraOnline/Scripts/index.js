c310ac013da174b0285aca4e8577ce4fa3144157
tafonso00 <1151046@isep.ipp.pt>
2023-06-01 23:15:02 +0100

    validacao do utlizador

--- BilheiteiraOnline/Scripts/index.js
+++ BilheiteiraOnline/Scripts/index.js
@@ -1,3 +1,34 @@
 function onClickHome(){
     window.location.href="index.html";
 }
+
+function validar(){
+
+        var name = document.forms["utilizador"]["UserName"];
+        var email = document.forms["utilizador"]["Email"];
+        var password = document.forms["utilizador"]["Password"];
+  
+        if (name.value == "") {
+            window.alert("Por favor insere o nome.");
+            name.focus();
+            return false;
+        }
+  
+        if (email.value == "")  {
+            window.alert("Por favor insere um email valido.");
+            email.focus();
+            return false;
+        }
+  
+        if (password.value == "") {
+            window.alert("Por favor insere uma password correcta");
+            password.focus();
+            return false;
+        
+        }
+  
+        return true;
+    }
+
+
+
