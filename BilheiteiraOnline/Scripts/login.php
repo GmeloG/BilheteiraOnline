92384ba362af9ea4425bbea5afb46e6a2fb4606e
GmeloG <goncalomelogoncalves@gmail.com>
2023-06-17 18:06:47 +0100

    trabalho final

--- BilheiteiraOnline/Scripts/login.php
+++ BilheiteiraOnline/Scripts/login.php
@@ -1,49 +1,59 @@
 <html>
-<head>Login</head>
+
+<head></head>
+
 <body>
-<?php
 
-$serverName = "127.0.0.1";
-$userName = "root";
-$ServerPassword = "";
-$databaseName = "bilheteira";
+    <?php
 
-$connection = mysqli_connect($serverName, $userName, $ServerPassword, $databaseName);
+    $serverName = "127.0.0.1";
+    $userName = "root";
+    $ServerPassword = "";
+    $databaseName = "bilheteira";
 
-if (!$connection) {
-die("Connection failed:" . mysqli_connect_error());
-}  
-echo "Connected successfully";
+    $connection = mysqli_connect($serverName, $userName, $ServerPassword, $databaseName);
 
-$email= $_GET['email'];
-$password= $_GET['password'];
+    if (!$connection) {
+        die("Connection failed:" . mysqli_connect_error());
+    }
+    echo "Connected successfully";
 
-$sql_query = "SELECT * FROM `utilizador` WHERE email = '$email' and password = '$password'";
-$result = mysqli_query($connection,$sql_query) or die(mysqli_error($connection));
-$numrows = mysqli_num_rows($result);
-if($numrows > 0)
-{
-    echo '<script>';
-    echo 'document.addEventListener("keyup", function(event) {';
-    echo 'if (event.keyCode === 13) {';
-    echo 'alert("BEmvindo utilizador");'.json_decode($email); 
-    echo 'window.location.href = "http://localhost/2parte/bilheteira/BilheiteiraOnline/scripts/ticket.php";'; 
-    echo '}';
-    echo '});';
-    echo '</script>';
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-}
-else
-{
-    echo '<script>';
-    echo  'window.alert("Este email não foi encontrado");
+    $sql_query = "SELECT * FROM `utilizador` WHERE email = '$email'";
+    $result = mysqli_query($connection, $sql_query) or die(mysqli_error($connection));
+    $numrows = mysqli_num_rows($result);
+
+    if ($numrows > 0) {
+
+        $row = mysqli_fetch_assoc($result);
+        $ServerPassword = $row['password'];
+
+        if ($password === $ServerPassword) {
+            echo '<script>';
+            echo  'window.alert("Bem Vindo: ' . $email . '");';
+            echo '</script>';
+        } else {
+            echo '<script>;';
+            echo 'window.alert("Password errada");';
+            echo '</script>';
+        }
+    } else {
+        echo '<script>';
+        echo  'window.alert("Este email não foi encontrado");
             window.location.href="http://localhost/2parte/BilheiteiraOnline/utilizador.html";';
-    echo '</script>';
-}
-?>
+        echo '</script>';
+        return false;
+    }
 
- 
-<script> onClickLogin()</script>
+    mysqli_close($connection);
+
+    ?>
+    <script>
+        window.location.href = "http://localhost/2parte/BilheiteiraOnline/index.html";
+    </script>
 
 </body>
+
 </html>
--- BilheiteiraOnline/Scripts/novoregisto.php
+++ BilheiteiraOnline/Scripts/novoregisto.php
@@ -1,41 +1,42 @@
 <html>
+
 <head></head>
+
 <body>
 
-<?php 
+    <?php
 
-    $serverName = "127.0.0.1"; 
-    $userName = "root"; 
-    $ServerPassword =""; 
+    $serverName = "127.0.0.1";
+    $userName = "root";
+    $ServerPassword = "";
     $databaseName = "bilheteira";
 
     $connection = mysqli_connect($serverName, $userName, $ServerPassword, $databaseName);
 
-    if(!$connection){
-        die ("Connection failed:" .mysqli_connect_error());
+    if (!$connection) {
+        die("Connection failed:" . mysqli_connect_error());
     }
     echo "Connected successfully";
-    
-    $nome= $_GET['nome'];
-    $email= $_GET['email'];
-    $password= $_GET['password'];
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
     $sql_query = "SELECT * FROM `utilizador` WHERE email = '$email';";
-    $result = mysqli_query($connection,$sql_query) or die(mysqli_error($connection));
+    $result = mysqli_query($connection, $sql_query) or die(mysqli_error($connection));
     $numrows = mysqli_num_rows($result);
-    if($numrows == 0)
-    {   
+    if ($numrows == 0) {
         $sql_query = "INSERT INTO `utilizador`(`nome`, `email`, `password`) VALUES ('$nome','$email','$password')";
 
-        if (mysqli_query($connection, $sql_query)){
-            echo "New User added!!";
+        if (mysqli_query($connection, $sql_query)) {
+            echo '<script>';
+            echo  'window.alert("Bem Vindo: ' . $email . '");';
+            echo '</script>';
+        } else {
+            echo "Error: " . $sql . "" . mysqli_error($connection);
         }
-        else{
-            echo "Error: " .$sql . "".mysqli_error($connection);
-        }
-    }    
-    else {
-      
+    } else {
+
         echo '<script>';
         echo  'window.alert("Já existe este email");
                 window.location.href="http://localhost/2parte/BilheiteiraOnline/utilizador.html";';
@@ -45,9 +46,12 @@
     mysqli_close($connection);
 
 
-?>
-<script>window.location.href="http://localhost/2parte/BilheiteiraOnline/index.html";</script>
+    ?>
+    <script>
+        window.location.href = "http://localhost/2parte/BilheiteiraOnline/index.html";
+    </script>
 
 
 </body>
+
 </html>
